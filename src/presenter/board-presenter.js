bb276e7f981f5c02fb135897690a08b79d1c3316
Maks <2102maksim2004@gmail.com>
2024-02-22 16:31:17 +0500

    module1-task3

---
+++ src/presenter/board-presenter.js
@@ -0,0 +1,28 @@
+import { render } from '../render.js';
+import EventsListView from '../view/events-list-view.js';
+import SortingView from '../view/sort-view.js';
+import EventItemView from '../view/element-view.js';
+import PointEditorView from '../view/editor-view.js';
+
+
+export default class Presenter {
+  eventsList = new EventsListView();
+  sortingComponent = new SortingView();
+
+  constructor({
+    tripEventsContainer
+  }) {
+    this.tripEventsContainer = tripEventsContainer;
+  }
+
+  init() {
+    render(this.sortingComponent, this.tripEventsContainer);
+    render(this.eventsList, this.tripEventsContainer);
+    render(new PointEditorView(), this.eventsList.getElement());
+
+    for (let i = 0; i < 3; i++) {
+      render(new EventItemView(), this.eventsList.getElement());
+    }
+  }
+}
+
---
+++ src/view/sort-view.js
@@ -0,0 +1,21 @@
+import { createSortingTemplate } from '../template/sort-template';
+import { createElement } from '../render.js';
+
+
+export default class SortingView {
+  getTemplate() {
+    return createSortingTemplate();
+  }
+
+  getElement() {
+    if (!this.element) {
+      this.element = createElement(this.getTemplate());
+    }
+
+    return this.element;
+  }
+
+  removeElement() {
+    this.element = null;
+  }
+}
